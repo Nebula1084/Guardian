28e8a64da08f9a6932e200b52f78cae501ca529d
Sredeption <hjw_redeption@outlook.com>
2017-11-14 22:51:06 +0800

    UI of ConfigFragment

--- app/src/main/java/hku/cs/smp/guardian/config/ConfigFragment.java
+++ app/src/main/java/hku/cs/smp/guardian/config/ConfigFragment.java
@@ -2,24 +2,28 @@ package hku.cs.smp.guardian.config;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.View;
-import android.view.ViewGroup;
 import hku.cs.smp.guardian.R;
 
-public class ConfigFragment extends Fragment {
+public class ConfigFragment extends PreferenceFragmentCompat {
     private View rootView;
 
     public static ConfigFragment newInstance() {
         return new ConfigFragment();
     }
 
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (rootView != null)
-            return rootView;
-        rootView = inflater.inflate(R.layout.frag_config, container, false);
-        return rootView;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_guardian);
     }
+
+    //    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        if (rootView != null)
+//            return rootView;
+//        rootView = inflater.inflate(R.layout.frag_config, container, false);
+//        return rootView;
+//    }
 }
